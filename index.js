74728d1a279694d031f99e41cb4ec13d7e6c1771
Puriti275 <puriti275@gmail.com>
2026-01-11 15:52:58 -0600

    Add files via upload
    Insanely basic functionality is completed.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d'); //c stands for context
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d') //c stands for context
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+const scoreElement = document.querySelector('#scoreEl')
+const bigScoreElement = document.querySelector('#bigScoreEl')
+const startGame = document.querySelector('#StartGame')
+const ui = document.querySelector('#beginning')
 
 class Player {
     constructor(x, y, radius, color) {
@@ -18,11 +23,245 @@ class Player {
         and ending angles, where Math.PI * 2 just equals 360 degrees. The false
         is asking to draw counterclockwise, which we are not.
         */
-        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
         c.fill()
     }
 }
 
-const player = new Player(100, 100, 30, 'blue')
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
+const friction = 0.99
+class Particle {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+        this.alpha = 1
+    }
+
+    draw() {
+        c.save()
+        c.globalAlpha = this.alpha
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+        c.restore()
+    }
+
+    update() {
+        this.draw()
+        this.velocity.x *= friction
+        this.velocity.y *= friction
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+        this.alpha -= 0.01
+    }
+}
+
+const x = canvas.width/2
+const y = canvas.height/2
+
+let player = new Player(x, y, 20, 'white')
+let projectiles = []
+let enemies = []
+let particles = []
+let score = 0;
+
+function init() {
+    player = new Player(x, y, 20, 'white')
+    projectiles = []
+    enemies = []
+    particles = []
+    score = 0;
+    scoreElement.innerHTML = score
+}
 
 player.draw();
+
+function spawnEnemies() {
+    setInterval(() => {
+        const radius = Math.random() * (30 - 4) + 8
+
+        let x
+        let y
+
+        if (Math.random() < 0.5) {
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+            y = Math.random() * canvas.height
+        }
+        else {
+            x = Math.random() * canvas.width
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+        }
+
+
+        const color = `hsl(${Math.random() * 360}, 50%, 50%)`
+
+        const angle = Math.atan2(
+            canvas.height/2 - y, canvas.width/2 - x)
+
+        const velocity = { 
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+    }, 1000) 
+}
+
+let animationID
+//infinite loop to constantly draw and update our projectiles
+function animate() {
+    animationID = requestAnimationFrame(animate)
+    c.fillStyle = 'rgba(0,0,0,0.1)'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.draw()
+
+    //animating particles
+    particles.forEach((particle, index) => {
+        if(particle.alpha <= 0) {
+            particles.splice(index, 1)
+        } else {
+            particle.update()
+        }
+    })
+
+    projectiles.forEach((projectile, index) => {
+        projectiles[index].update()
+
+        //removes from edges of screen
+        if(projectile.x + projectile.radius < 0 ||
+            projectile.x - projectile.radius > canvas.width ||
+            projectile.y + projectile.radius < 0 ||
+            projectile.y - projectile.radius > canvas.height) {
+            setTimeout(() => {
+                projectiles.splice(index, 1)
+            }, 0);
+        }
+    })
+
+    enemies.forEach((enemy, index) => {
+        enemy.update()
+
+        const distance = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+
+        if(distance - enemy.radius - player.radius < 1) {
+            //end game
+            cancelAnimationFrame(animationID)
+            ui.style.display = "flex"
+            bigScoreElement.innerHTML = score
+        }
+
+        projectiles.forEach((projectile, projectileIndex) => {
+            const distance = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+
+            // when projectiles touch enemies
+            if(distance - enemy.radius - projectile.radius < 1) {
+
+                //creating particles
+                for(let i = 0; i < enemy.radius * 2; i++) {
+                    particles.push(new Particle(
+                        projectile.x, 
+                        projectile.y, 
+                        Math.random() * 2,
+                        enemy.color, 
+                        {x: (Math.random() - 0.5) * (Math.random() * 8), y: (Math.random() - 0.5) * (Math.random() * 8)}))
+                }
+
+                if (enemy.radius - 4 > 10) {
+                    score += 10
+                    scoreElement.innerHTML = score
+
+                    gsap.to(enemy, {
+                        radius: enemy.radius - 10
+                    })
+                    projectiles.splice(projectileIndex)
+                } else {
+                    setTimeout(() => {
+                        enemies.splice(index, 1)
+                        projectiles.splice(projectileIndex, 1)
+                    }, 0);
+                    
+                    score += 30;
+                    scoreElement.innerHTML = score
+                }
+            }
+        })
+    })
+
+}
+
+window.addEventListener('click', (event) => {
+    const angle = Math.atan2(
+        event.clientY - canvas.height/2, event.clientX - canvas.width/2)
+
+    const velocity = {
+        x: Math.cos(angle) * 3,
+        y: Math.sin(angle) * 3
+    }
+
+    projectiles.push(new Projectile(
+        canvas.width/2, canvas.height/2, 5, 'white', velocity
+    ))
+})
+
+startGame.addEventListener('click', (event) => {
+    init()
+    animate()
+    spawnEnemies()
+
+    ui.style.display = "none"
+})
+
+/*
+animate()
+spawnEnemies()
+*/
